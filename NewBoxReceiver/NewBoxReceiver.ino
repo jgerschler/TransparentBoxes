c537becb1dc905358ad2a3e7739e1b0ce0e63934
Jared Gerschler <jjgerschler@gmail.com>
2020-03-12 07:05:16 -0700

    added code for radio setup

--- NewBoxReceiver/NewBoxReceiver.ino
+++ NewBoxReceiver/NewBoxReceiver.ino
@@ -1,6 +1,19 @@
 
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8);
+
+const byte address[6] = "00001";
 
 void setup() {
+  //Receiver Setup
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
   //Illuminated Buttons
   pinMode(39, OUTPUT);
   pinMode(40, OUTPUT);
